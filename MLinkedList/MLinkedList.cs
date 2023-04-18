10472824a9fe945567ec7fa95740a1941ad25779
Михаил <misha13022008@gmail.com>
2023-04-18 22:11:36 +0300

    Add, Find, Contains, Last

---
+++ MLinkedList/MLinkedList.cs
@@ -0,0 +1,104 @@
+﻿namespace MLinkedList;
+
+using System.Text;
+
+public class MLinkedList<T> 
+{
+    private Node<T>? _start;
+
+    public Node<T>? Last
+    {
+        get
+        {
+            Node<T>? currentNode = _start;
+            while (currentNode?.Next != null) currentNode = currentNode.Next;
+            return currentNode;
+        }
+    }
+
+    public void Add(T obj)
+    {
+        if (_start == null)
+        {
+            _start = new Node<T>(obj, null);
+        }
+        else
+        {
+            Node<T> currentNode = _start;
+            while (currentNode.Next != null) currentNode = currentNode.Next;
+            currentNode.Next = new Node<T>(obj, null);
+        }
+    }
+
+    public T Find(Predicate<T> predicate)
+    {
+        if (_start == null) ThrowHelper.ThrowNotFoundException();
+
+        Node<T>? currentNode = _start!;
+        do
+        {
+            if (predicate(currentNode.Value))
+                return currentNode.Value;
+            currentNode = currentNode.Next;
+        } while (currentNode != null);
+
+        ThrowHelper.ThrowNotFoundException();
+        return default!;
+    }
+
+    public bool Contains(Predicate<T> predicate)
+    {
+        if (_start == null) return false;
+
+        Node<T>? currentNode = _start!;
+        do
+        {
+            if (predicate(currentNode.Value))
+                return true;
+            currentNode = currentNode.Next;
+        } while (currentNode != null);
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new(32);
+        builder.Append('[');
+
+        if (_start == null) goto end;
+
+        Node<T>? currentNode = _start!;
+        do
+        {
+            builder.Append(currentNode.Value);
+            builder.Append(',');
+            currentNode = currentNode.Next;
+        } while (currentNode != null);
+
+        end:
+        builder.Append(']');
+        return builder.ToString();
+    }
+}
+
+public static class ThrowHelper
+{
+    public static void ThrowNotFoundException()
+    {
+        throw new MLinkedListException("Value was not found");
+    }
+}
+
+public class MLinkedListException : Exception
+{
+    public MLinkedListException(string message) : base(message)
+    {
+    }
+}
+
+public record Node<T>(T Value, Node<T>? Next)
+{
+    public readonly T Value = Value;
+    public Node<T>? Next = Next;
+}
---
+++ MLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MLinkedList;
+
+public static class Program
+{
+    private static void Main()
+    {
+        MLinkedList<int> myLinkedList = new();
+        LinkedList<int> linkedList = new();
+            
+        myLinkedList.Add(1);
+        myLinkedList.Add(2);
+        myLinkedList.Add(5);
+
+        string s = myLinkedList.ToString();
+        
+        Console.WriteLine(myLinkedList.Find((a) => a == 2));
+        Console.WriteLine(myLinkedList.Contains((a) => a == 2));
+        Console.WriteLine(myLinkedList);
+        Console.WriteLine(myLinkedList.Last);
+        Console.WriteLine(new MLinkedList<int>().Last);
+    }
+}
